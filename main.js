56db0c2c5e7ac578a06a09c6b20c73d459a3312d
Shahrombek <shahromfozilov02@gmail.com>
2021-12-17 14:45:10 +0500

    yangilandi

--- main.js
+++ main.js
@@ -7,26 +7,26 @@ const player = document.getElementById("player");
 const player1 = document.getElementById("player1");
 
 let randomNumber = Math.floor(Math.random() * 100) + 1;
-console.log(randomNumber);
 number.focus();
 
 let count = 10;
 
 function check() {
     count--;
+    // agar son kiritmasa bajariladigan shart
     if (number.value == "") {
         count++;
         democha.innerHTML = "";
     }
-
+    // to'g'ri topgan holatda bajariladigan sayt
     else if (randomNumber == number.value) {
         succes.style.display = "block";
-
-        // g'olib divga stil berish
         
         //G'olib mp3ni yoqish
         player.play();
     }
+
+    // noto'g'ri topgan holatda bajariladigan sayt
     else if (count == 0 && succes.style.display !== "block") {
         unsucces.style.display = "block";
 
@@ -34,9 +34,12 @@ function check() {
         player1.play();
     }
 
+    // agar kiritilgan son kompyuter o'ylagan sondan katta bo'lsa bajariladigan shart
     else if (randomNumber > number.value) {
         democha.innerHTML = "Siz kiritgan son kichik";
     }
+    
+    // agar kiritilgan son kompyuter o'ylagan sondan kichik bo'lsa bajariladigan shart
     else if (randomNumber < number.value) {
         democha.innerHTML = "Siz kiritgan son katta";
     }
@@ -45,15 +48,21 @@ function check() {
 }
 
 
+// modal oyna bosilganda bajariladigan funksiya
 function chesk() {
     count = 10;
     num.innerHTML = count;
 
+    // yangi random son tanlash
     randomNumber = Math.floor(Math.random() * 100) + 1;
 
+    // yordam tegini yashirish
     democha.innerHTML = "";
+
+    // input tegidagi sonni tozalash
     number.value = '';
 
+    // o'yin qayta boshlandandagi modallar holati
     succes.style.display = "none";
     unsucces.style.display = "none";
 
